4da46bba6f27df850e75d6828a16f0f2154516d5
rmichel1115 <rcmichel@ncsu.edu>
2024-12-02 12:37:54 -0500

    Update sim_proc.c

--- sim_proc.c
+++ sim_proc.c
@@ -36,18 +36,34 @@ typedef struct {
 } ProcessorInformation;
 
 # define MAXIMUM 1000000
-
+// double instructions_per_cycle = 0;
+// uint64_t track_instructions = 0;
+// uint64_t track_cycle = 0;
+// long int finish_file = 0;
+// PL processor_PL;
 
 long int instruction_tracker = 0;
 long int instruction_stop = 0;
 long int PL_cycle = 0;
 ProcessorInformation read_instruction[MAXIMUM];
+//function below begins the pipeline 
 
-
+// void PL_Start(proc_params params){
+//     processor_PL.width = params.width;
+//     processor_PL.iq_size = params.iq_size;
+//     processor_PL.rob_size = params.rob_size; 
+//     processor_PL.PL_cycle = 0;
+//     processor_PL.CO_read = malloc(params.rob_size * sizeof(PL_instructions));
+//     processor_PL.EX_read = malloc(params.width * sizeof(PL_instructions));
+//     processor_PL.IS_read = malloc(params.rob_size * sizeof(PL_instructions));
+//     processor_PL.FE_read = malloc(params.width * sizeof(PL_instructions));
+// }
 
 //defining functions for pipeline simulation (reverse order)
 
-
+// void Commit(){
+//     printf("Instructions Committed\n");
+// }
 
 void Writeback() {
     if (instruction_tracker > 6)
@@ -151,7 +167,11 @@ int main (int argc, char* argv[])
     }
       printf("=== Simulator Command ======\n# ./sim %lu %lu %lu %s\n", params.rob_size, params.iq_size, params.width, argv[4]);
       printf("=== Processor Configuration ===\n# ROB_SIZE = %lu\n# IQ_SIZE = %lu\n# WIDTH = %lu\n", params.rob_size, params.iq_size, params.width);
-
+  
+   
+    for (int j = 0; j < instruction_tracker; j++) {
+        printf("%d fu{%d} src{%d,%d} dst{%d} FE{%d,1} DE{%d,1} RN{%d,1} RR{%d,1} DI{%d,1} IS{%d,1} EX{%d,1} WB{%d,1} RT{%d,1}\n", j, read_instruction[j].INSTR, read_instruction[j].src1, read_instruction[j].src2, read_instruction[j].dest, read_instruction[j].FETCH, read_instruction[j].DECODE, read_instruction[j].RENAME, read_instruction[j].REGREAD, read_instruction[j].DISPATCH, read_instruction[j].ISSUE, read_instruction[j].EXECUTE, read_instruction[j].WRITEBACK, read_instruction[j].RETIRE);
+    }
    do {
     Retire();
     Writeback();
@@ -169,10 +189,7 @@ int main (int argc, char* argv[])
     printf(" Dynamic Instruction Count = %d\n", instruction_tracker);
     printf(" Cycles = %d\n", PL_cycle);
     printf(" Instructions Per Cycle (IPC) = %.2f\n", instruction_tracker / PL_cycle);   
-    
-    for (int j = 0; j < instruction_tracker; j++) {
-        printf("%d fu{%d} src{%d,%d} dst{%d} FE{%d,1} DE{%d,1} RN{%d,1} RR{%d,1} DI{%d,1} IS{%d,1} EX{%d,1} WB{%d,1} RT{%d,1}\n", j, read_instruction[j].INSTR, read_instruction[j].src1, read_instruction[j].src2, read_instruction[j].dest, read_instruction[j].FETCH, read_instruction[j].DECODE, read_instruction[j].RENAME, read_instruction[j].REGREAD, read_instruction[j].DISPATCH, read_instruction[j].ISSUE, read_instruction[j].EXECUTE, read_instruction[j].WRITEBACK, read_instruction[j].RETIRE);
-    }
+  
     
     //while(fscanf(FP, "%lx %d %d %d %d", &pc, &op_type, &dest, &src1, &src2) != EOF)
         //printf("%lx %d %d %d %d\n", pc, op_type, dest, src1, src2); //Print to check if inputs have been read correctly
