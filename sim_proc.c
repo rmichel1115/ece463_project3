b0f4cace1023db0196e5500ce61bc50a6b712410
rmichel1115 <rcmichel@ncsu.edu>
2024-12-02 12:34:41 -0500

    Update sim_proc.c

--- sim_proc.c
+++ sim_proc.c
@@ -16,65 +16,109 @@
     ... and so on
 */
 
-//intialize variables 
-double instructions_per_cycle = 0;
-uint64_t track_instructions = 0;
-uint64_t track_cycle = 0;
-long int finish_file = 0;
-PL processor_PL;
+//steps of the pipeline instructions 
+typedef struct {
+    long int INSTR;
+    long int src1;
+    long int src2;
+    long int dest;
+    long int FETCH;
+    long int DECODE;
+    long int REGREAD;
+    long int RENAME;
+    long int WRITEBACK;
+    long int RETIRE;
+    long int EXECUTE;
+    long int ISSUE;
+    long int DISPATCH;
+    uint64_t pc;
 
-//function below begins the pipeline 
+} ProcessorInformation;
 
-void PL_Start(proc_params params){
-    processor_PL.width = params.width;
-    processor_PL.iq_size = params.iq_size;
-    processor_PL.rob_size = params.rob_size; 
-    processor_PL.PL_cycle = 0;
-    processor_PL.CO_read = malloc(params.rob_size * sizeof(PL_instructions));
-    processor_PL.EX_read = malloc(params.width * sizeof(PL_instructions));
-    processor_PL.IS_read = malloc(params.rob_size * sizeof(PL_instructions));
-    processor_PL.FE_read = malloc(params.width * sizeof(PL_instructions));
-}
+# define MAXIMUM 1000000
+
+
+long int instruction_tracker = 0;
+long int instruction_stop = 0;
+long int PL_cycle = 0;
+ProcessorInformation read_instruction[MAXIMUM];
+
+
 
 //defining functions for pipeline simulation (reverse order)
 
-void Commit(){
-    printf("Instructions Committed\n");
-}
+
+
+void Writeback() {
+    if (instruction_tracker > 6)
+    read_instruction[instruction_stop].WRITEBACK = PL_cycle;
+    }
 
 void Execute() {
-    printf("Instructions Executed\n");
-}
+    if (instruction_tracker > 5)
+    read_instruction[instruction_stop].EXECUTE = PL_cycle;
+    }
 
-void Issue(proc_params params){
-    printf("%lu instructions\n", params.width / 2);
-}
 
-void Decode(proc_params params){
-    printf("%lu instructions\n", params.width);
+void Issue() {
+    if (instruction_tracker > 4)
+    read_instruction[instruction_stop].ISSUE = PL_cycle;
+    }
+
+void Decode(){
+    if (instruction_tracker > 0 )
+    read_instruction[instruction_stop].DECODE = PL_cycle;
+
 
 }
 
 void Fetch(FILE *FP, proc_params params){
-    for (int j = 0; j < !finish_file && params.width; j++) {
-        if (scanf(FP, "%lx %d %d %d %d", &processor_PL.FE_read[j].pc, &processor_PL.FE_read[j].op_type, &processor_PL.FE_read[j].dest, &processor_PL.FE_read[j].src1, &processor_PL.FE_read[j].src2) == EOF){
-            finish_file = 1;
-        }
-            
+if (fscanf(FP, "%lx %d %d %d %d", &read_instruction[instruction_tracker].pc, &read_instruction[instruction_tracker].INSTR, &read_instruction[instruction_tracker].dest, &read_instruction[instruction_tracker].src1, &read_instruction[instruction_tracker].src2) != EOF){
+    instruction_tracker++;
+    read_instruction[instruction_tracker].FETCH = PL_cycle;
+}
+
+}
+
+
+void Dispatch() {
+    if (instruction_tracker > 3)
+    read_instruction[instruction_stop].DISPATCH = PL_cycle;
+    }
+
+void RegRead() {
+    if (instruction_tracker > 2)
+    read_instruction[instruction_stop].REGREAD = PL_cycle;
+    }
+
+
+void Rename() {
+    if (instruction_tracker > 1)
+    read_instruction[instruction_stop].RENAME = PL_cycle;
+    }
+
+void Retire() {
+    if (instruction_tracker > instruction_stop) {
+        read_instruction[instruction_stop].RETIRE = PL_cycle;
+        instruction_stop++;
     }
 }
+ //recall pipeline stages in reverse order 
+int Advance_Cycle() {
 
-int Advance_Cycle(FILE *FP, proc_params params) {
-
-    Commit();
-    Execute();
-    Issue(params);
-    Decode(params);
-    Fetch(FP, params);
-    processor_PL.PL_cycle++; //counter for pipeline
-    return !finish_file;
-
+ PL_cycle++;
+ Retire();
+ Writeback();
+ Execute();
+ Issue();
+ Dispatch();
+ RegRead();
+ Rename(); 
+ Decode();
+ Fetch(NULL, (proc_params){0});
+ return instruction_stop < instruction_tracker;
 }
+
 int main (int argc, char* argv[])
 {
     FILE *FP;               // File handler
@@ -105,21 +149,34 @@ int main (int argc, char* argv[])
         printf("Error: Unable to open file %s\n", trace_file);
         exit(EXIT_FAILURE);
     }
-   
-   PL_Start(params);
+      printf("=== Simulator Command ======\n# ./sim %lu %lu %lu %s\n", params.rob_size, params.iq_size, params.width, argv[4]);
+      printf("=== Processor Configuration ===\n# ROB_SIZE = %lu\n# IQ_SIZE = %lu\n# WIDTH = %lu\n", params.rob_size, params.iq_size, params.width);
+
    do {
-    Advance_Cycle(FP, params);
-    track_instructions += params.width;
-   
-  } while (!finish_file);
+    Retire();
+    Writeback();
+    Execute();
+    Issue();
+    Dispatch();
+    RegRead();
+    Rename();
+    Decode();
+    Fetch(FP, params);
 
-    instructions_per_cycle = track_instructions / processor_PL.PL_cycle;
+   } while (Advance_Cycle());
 
-    printf("Cycles: %lu Instructions: %.u Instructions Per Cycle: %.2f\n", processor_PL.PL_cycle, track_instructions, instructions_per_cycle);
-    fclose(FP); 
+    printf("=== Simulation Results =======\n");
+    printf(" Dynamic Instruction Count = %d\n", instruction_tracker);
+    printf(" Cycles = %d\n", PL_cycle);
+    printf(" Instructions Per Cycle (IPC) = %.2f\n", instruction_tracker / PL_cycle);   
+    
+    for (int j = 0; j < instruction_tracker; j++) {
+        printf("%d fu{%d} src{%d,%d} dst{%d} FE{%d,1} DE{%d,1} RN{%d,1} RR{%d,1} DI{%d,1} IS{%d,1} EX{%d,1} WB{%d,1} RT{%d,1}\n", j, read_instruction[j].INSTR, read_instruction[j].src1, read_instruction[j].src2, read_instruction[j].dest, read_instruction[j].FETCH, read_instruction[j].DECODE, read_instruction[j].RENAME, read_instruction[j].REGREAD, read_instruction[j].DISPATCH, read_instruction[j].ISSUE, read_instruction[j].EXECUTE, read_instruction[j].WRITEBACK, read_instruction[j].RETIRE);
+    }
+    
     //while(fscanf(FP, "%lx %d %d %d %d", &pc, &op_type, &dest, &src1, &src2) != EOF)
         //printf("%lx %d %d %d %d\n", pc, op_type, dest, src1, src2); //Print to check if inputs have been read correctly
-
+    fclose(FP);
     return 0;
 }
 
